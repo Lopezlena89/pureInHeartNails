dc9a39cc547c3ca65a6a8708d9c5fa70767864a6
Luis Mariano Lopez Lena Valdivieso <luismllv89@gmail.com>
2024-02-19 18:16:25 -0600

    location about

--- src/components/AboutPage.tsx
+++ src/components/AboutPage.tsx
@@ -1,6 +1,36 @@
+import { useLocation } from "react-router-dom";
+import { Footer } from "./Footer"
+import { Navbar } from "./ui/Navbar"
 
 export const AboutPage = () => {
+  const location = useLocation();
+
   return (
-    <div>AboutPage</div>
+    <div className="w-full h-full">
+    <Navbar path={location.pathname}/>
+    <div className="w-full h-[800px] bg-gray-100 flex ">
+        <div className="w-1/2 h-full flex justify-center items-center">
+          <div className="w-2/3 h-4/6 mt-20 flex flex-col font-thin text-gray-600">
+              <h2 className="h-20 text-4xl">ABOUT US</h2>
+              <p className="h-36 mt-5">A lot of these new "hype" up and coming nail salons claim to revolutionize the nail 
+                industry with new claimed procedures, fancy products, and a new culture that is 
+                supposedly better than what used to be in the past.</p>
+              <p className="mt-5">We are not trying to be any of those things.</p>
+              <p className="mt-5">As an experienced salon operator for 8+ years in the DMV area, our core focus is this: </p>
+              <p className="mt-5">To provide the <span className="font-semibold">highest quality </span>
+              services and experience while keeping the cost affordable 
+              and easy to reach for everyone.</p>
+              <p className="mt-5">We learned that no matter how much buzzwords you try to put into a nail salon, if you 
+                can't back it up with a great end product that lasts, it is useless.</p>
+              <p className="mt-5">So, with all this in mind, we are constantly striving to create an experience that you, 
+                the client, will be extremely happy with. That is who we are at Pure in Heart.</p>
+          </div>
+        </div>
+        <div className="w-1/2 h-full bg-[url('/nails-picture.png')] bg-cover">
+
+        </div>
+    </div>
+    <Footer/>
+</div>
   )
 }
--- src/components/LocationsPage.tsx
+++ src/components/LocationsPage.tsx
@@ -1,7 +1,32 @@
+import { useLocation } from "react-router-dom";
+import { Navbar } from "./ui/Navbar"
+import { Footer } from './Footer';
 
 
 export const LocationsPage = () => {
+  const location = useLocation();
+
+
   return (
-    <div>LocationsPage</div>
+    <div className="w-full h-full">
+        <Navbar path={location.pathname}/>
+        <div className="w-full h-[800px] bg-gray-100 flex flex-col justify-around items-center">
+            <div className="w-full h-[200px] flex flex-col justify-center items-center mt-20">
+                <h2 className="font-thin text-gray-600 text-5xl mt-20">OUR LOCATIONS</h2>
+                <p className="font-thin text-gray-600 mt-10">Washington DC</p>
+            </div>
+            <div className="w-full h-[600px] flex justify-center">
+              <div className="w-1/2 full flex flex-col justify-center items-center">
+                  <div className="w-[300px] h-[300px] rounded-full bg-[url('/store.png')] bg-cover"/>
+                  <h4 className="text-gray-500 mt-10 font-thin text-4xl">SHAW</h4>
+              </div>
+              <div className="w-1/2 full flex flex-col justify-center items-center">
+                  <div className="w-[300px] h-[300px] rounded-full bg-[url('/Pedicure.png')] bg-cover"/>
+                  <h4 className="text-gray-500 mt-10 font-thin text-4xl">BERKELEY / PALISADES</h4>
+              </div>
+            </div>
+        </div>
+        <Footer/>
+    </div>
   )
 }
